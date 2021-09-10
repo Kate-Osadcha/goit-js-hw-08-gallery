445236fe0a5b2a4183bb2217f84e087505635c3c
Катерина Осадчая <monzolevskaya@gmail.com>
2021-09-10 23:55:19 +0300

    Пролистывание клавишами "влево" и "вправо".

--- gallery-images.js
+++ gallery-images.js
@@ -4,47 +4,38 @@ const galleryItemsRef = document.querySelector(".js-gallery");
 const divLightboxRef = document.querySelector(".js-lightbox");
 const imgLightboxRef = document.querySelector(".lightbox__image");
 
-
-const createItemsRef = (elements) => {
-  return elements
-    .map(({ preview, original, description }) => {
+const createItemsRef = galleryItems
+    .map(({ preview, original, description }, index) => {
       return `<li class="gallery__item">
       <a class="gallery__link"
       href = "${original}">
         <img class="gallery__image"
         src = "${preview}" 
         data-source = "${original}" 
-        alt ="${description}"/>
+        alt ="${description}"
+        data-index="${index}"/>
+        
        </a>
     </li>`;
     })
     .join("");
-};
-galleryItemsRef.insertAdjacentHTML("afterbegin", createItemsRef(galleryItems));
 
+galleryItemsRef.innerHTML=createItemsRef
 
 const openModalClick = (e) => {
   e.preventDefault();
 
-  if (e.target.nodeName === "IMG") {
+  if (e.target.localName === "img") {
     imgLightboxRef.src = e.target.dataset.source;
     imgLightboxRef.alt = e.target.alt;
+    imgLightboxRef.dataset.index = e.target.dataset.index;
     divLightboxRef.classList.add("is-open");
   }
 };
 galleryItemsRef.addEventListener("click", openModalClick);
 
-
-const escapeClick = e => {
-  if (e.key === "Escape") {
-    divLightboxRef.classList.remove("is-open");
-  }
-};
-window.addEventListener("keyup", escapeClick);
-
-
-const closeModalClick = e => {
-  if (e.target.nodeName !== "IMG") {
+const closeModalClick = (e) => {
+  if (e.target.localName !== "img") {
     divLightboxRef.classList.remove("is-open");
 
     imgLightboxRef.src = "";
@@ -52,3 +43,48 @@ const closeModalClick = e => {
   }
 };
 window.addEventListener("click", closeModalClick);
+
+const escapeClick = (e) => {
+  if (e.key === "Escape") {
+    divLightboxRef.classList.remove("is-open");
+  }
+};
+window.addEventListener("keyup", escapeClick);
+
+
+function indexCurretImg(index, step = 0) {
+  imgLightboxRef.dataset.index = `${index + step}`;
+  imgLightboxRef.src = galleryItems[index + step].original;
+  imgLightboxRef.alt = galleryItems[index + step].description;
+}
+
+
+function clickArrowRight() {
+  let index = +imgLightboxRef.dataset.index;
+  if (index === galleryItems.length - 1) {
+    indexCurretImg(0);
+    return;
+  }
+  indexCurretImg(index, 1);
+}
+
+
+function clickArrowLeft() {
+  let index = +imgLightboxRef.dataset.index;
+
+  if (index === 0) {
+    indexCurretImg(galleryItems.length - 1);
+    return;
+  }
+  indexCurretImg(index, -1);
+}
+
+
+window.addEventListener("keydown", (e) => {
+  if (e.code === "ArrowRight") {
+    clickArrowRight();
+  }
+  if (e.code === "ArrowLeft") {
+    clickArrowLeft();
+  }
+});
